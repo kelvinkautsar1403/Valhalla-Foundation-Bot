71ee5902afcbf8585bbf95d024389374b9005585
Kelvin Kautsar <114674454+kelvinkautsar1403@users.noreply.github.com>
2024-05-21 15:02:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const accounts = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
 (async () => {
   process.stdout.write('\x1Bc');
   console.log('================================================'.blue);
-  console.log('=        Valhalla Foundation Bot               ='.blue);
-  console.log('=      Created by AirdropGemHunter             ='.blue);
-  console.log('=     https://t.me/AirdropGemHunterChannel     ='.blue);
+  console.log('=            Valhalla Foundation Bot           ='.blue);
+  console.log('=         Created by AirdropGemHunter          ='.blue);
+  console.log('=      https://t.me/AirdropGemHunterChannel    ='.blue);
   console.log('================================================'.blue);
   console.log();
   try {
